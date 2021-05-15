7bf44ea1535f4deeabe417cdf3540362c1307860
Daniel Wolfson <47075191+daniel-wolfson@users.noreply.github.com>
2021-05-15 18:45:44 +0300

    add validators

---
+++ Validators/ApiRequestValidator.cs
@@ -0,0 +1,26 @@
+﻿using Crpm.Infrastructure.Interfaces;
+using Crpm.Model.Data;
+using FluentValidation;
+
+namespace Crpm.Infrastructure.Validators
+{
+    class ApiRequestValidator : AbstractValidator<UserDetails>
+    {
+        public ApiRequestValidator()
+        {
+            RuleFor(x => x.UserGuid)
+                .NotEmpty()
+                //.Must
+                .NotNull();
+
+            //public List<EntityDescription> entity_description_list { get; set; }
+            //public List<Descriptions> description_list { get; set; }
+            //public List<ConvertionTable> convertion_table { get; set; }
+            //public List<MeasuringUnit> measuring_unit { get; set; }
+            //public List<CalenderRollup> calender_rollup { get; set; }
+            //public List<RollupMethod> rollup_method { get; set; }
+            //public List<string> model_orgs_list { get; set; }
+            //public List<Connected_Model> connected_model_list { get; set; }
+        }
+    }
+}
