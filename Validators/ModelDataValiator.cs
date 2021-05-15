4114159512c09039c13f998b479576ee4df8a57e
Daniel Wolfson <47075191+daniel-wolfson@users.noreply.github.com>
2021-05-15 18:50:21 +0300

    add validators

---
+++ Validators/ModelDataValiator.cs
@@ -0,0 +1,22 @@
+﻿using Crpm.Model.Data;
+using FluentValidation;
+
+namespace Crpm.Dal.Validators
+{
+    public class ModelDataValiator2 : AbstractValidator<ModelData>
+    {
+        public ModelDataValiator2()
+        {
+            //RuleFor(x => x.entity_description_list).NotNull();
+
+            //public List<EntityDescription> entity_description_list { get; set; }
+            //public List<Descriptions> description_list { get; set; }
+            //public List<ConvertionTable> convertion_table { get; set; }
+            //public List<MeasuringUnit> measuring_unit { get; set; }
+            //public List<CalenderRollup> calender_rollup { get; set; }
+            //public List<RollupMethod> rollup_method { get; set; }
+            //public List<string> model_orgs_list { get; set; }
+            //public List<Connected_Model> connected_model_list { get; set; }
+        }
+    }
+}
---
+++ Validators/UserDetailsValiator.cs
@@ -0,0 +1,14 @@
+﻿using Crpm.Model.Data;
+using FluentValidation;
+
+namespace Crpm.Dal.Validators
+{
+    public class UserDetailsValiator2 : AbstractValidator<UserDetails>
+    {
+        public UserDetailsValiator2()
+        {
+            RuleFor(x => x.UserGuid).NotEmpty().NotNull();
+            RuleFor(x => x.Password).NotEmpty().NotNull();
+        }
+    }
+}
